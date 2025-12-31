ab65e2cfc8ba338cd8951b10c3d2ead0ed5e0f3c
richard-carvalho-04 <richardcarvalho0123@gmail.com>
2025-12-31 11:45:48 +0530

    triggering the deployment

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'//we cannot give any other name to it it's the default name 
 import Header from './Components/Header/Header.jsx'
-import Footer from './Components/Footer/Footer.jsx'
+import Footer from './Components/Footer/Footer.jsx'//trying to trigger the deployment so i made this comment
 
 function Layout() {
   return (
